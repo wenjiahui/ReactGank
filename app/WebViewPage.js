aa2a99d100d301fe272d958b3a07e63aaaa03ca1
Jiahui.wen <jiahuiwen622111@gmail.com>
2017-09-07 15:22:18 +0800

    add share action

--- app/WebViewPage.js
+++ app/WebViewPage.js
@@ -1,21 +1,54 @@
 import React from 'react'
 
-import { View, WebView, StyleSheet, ProgressBarAndroid } from 'react-native';
+import {
+  View, WebView, TouchableOpacity, Text, StyleSheet, Share, ProgressBarAndroid
+} from 'react-native';
 import { default_primary_color, window_color, accent_color } from "./color_palette";
 
 class WebViewPage extends React.Component {
 
-  static navigationOptions = ({ navigation, screenProps }) => ({
-    title: navigation.state.params.info.desc,
-    headerTintColor: 'white',
-    headerStyle: { backgroundColor: default_primary_color }
-  });
+  static navigationOptions = ({ navigation, screenProps }) => {
+    const { state } = navigation;
+    const { renderHeaderRight } = state.params;
+    return {
+      title: navigation.state.params.info.desc,
+      headerTintColor: 'white',
+      headerStyle: { backgroundColor: default_primary_color },
+      headerRight: renderHeaderRight && renderHeaderRight()
+    }
+  };
 
   constructor(props) {
     super(props);
     this.state = {
       loading: true
     }
+  }
+
+  componentWillMount() {
+    this.props.navigation.setParams({ clickShare: this._onShare })
+    this.props.navigation.setParams({
+      renderHeaderRight: () => (
+        <TouchableOpacity onPress={this._onShare.bind(this)}>
+          <Text style={{
+            color: 'white',
+            paddingRight: 10,
+            fontSize: 15
+          }}>分享</Text>
+        </TouchableOpacity>)
+    });
+  }
+
+  _onShare() {
+    const { params } = this.props.navigation.state;
+    const info = params.info;
+
+    Share.share({
+      title: info.desc,
+      message: '我在浏览文章《' + info.desc + "》,你也快来看看吧。" + info.url
+    }).then(response => {
+
+    })
   }
 
   render() {
